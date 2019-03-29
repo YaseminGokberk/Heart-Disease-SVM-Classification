5a94e0a3aeed52708e9b091d48bf346f8ef3cd9d
Yasemin Gökberk <yasemingokberk@gmail.com>
2019-03-29 16:32:37 +0300

    Update svm_heart_disease.py

--- svm_heart_disease.py
+++ svm_heart_disease.py
@@ -7,6 +7,23 @@ Created on Thu Mar 28 13:56:35 2019
 
 """
  HEART DISEASE - SVM
+ 
+ "Target" alanı, hastada kalp hastalığının varlığını ifade eder.
+ age: yas
+ sex : (1 = erkek; 0 = kadın)
+ cp: gogus acı tipi
+ trestbps: dinlenme kan basıncı (in mm Hg on admission to the hospital)
+ chol: serum kolesterol ( mg/dl )
+ fbs: (açken kan sekeri > 120 mg/dl) (1 = true; 0 = false)
+ restecg: dinlenme elektrokardiyografik sonuçlar
+ thalach: elde edilen maksimum kalp atış hızı
+ exang: exercise indüklenmiş anjina (1 = yes; 0 = no)
+ oldpeak: ST depression induced by exercise relative to rest
+ slope: the slope of the peak exercise ST segment
+ ca: number of major vessels (0-3) colored by flourosopy
+ thal: 3 = normal; 6 = sabit kusur; 7 = tersinir kusur
+ target: 1 or 0
+ 
 """
 import numpy as np
 import pandas as pd
@@ -15,6 +32,7 @@ import matplotlib.mlab as mlab
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import roc_curve
 from sklearn.svm import SVC
 import itertools
 import seaborn
@@ -48,6 +66,7 @@ def plot_confusion_matrix(cm, classes,
 data = pd.read_csv('heart_disease.csv')
 data.head()
 data.target.value_counts()
+# target degerleri ve sayıları
 
 seaborn.countplot(x="target", data=data, palette="bwr")
 
@@ -86,7 +105,7 @@ no_heart_disease = y[y[:,0]== 0]
 no_heart_disease = len(no_heart_disease)
 heart_disease = y[y[:,0]==1]
 heart_disease = len(heart_disease)
-print("Sıfırlar: "+str(no_heart_disease)+"\nBirler: "+str(heart_disease))
+print("Sınıf 0: "+str(no_heart_disease)+"\nSınıf 1: "+str(heart_disease))
 
 
 # MODEL -> SVM CLASSIFICATION (kernel : linear)
@@ -103,16 +122,14 @@ plot_confusion_matrix(cm_ln, class_names)
 
 #print("Accuracy: "+str( (cm_ln[0][0]+cm_ln[1][1]) / (sum(cm_ln[0])+sum(cm_ln[1])) ) )
 print("Test Accuracy of SVM Algorithm (Linear): {:.2f}%".format(classification_ln.score(x_test,y_test)*100))
+print("TPR rate (Sensitivity-Recall): "+ str(cm_ln[1][1]) +" / "+str(cm_ln[1][0]+cm_ln[1][1])+" = "+str(cm_ln[1][1]/(cm_ln[1][0]+cm_ln[1][1]) )  ) 
+
 
 # MODEL -> SVM CLASSIFICATION (kernel : rbf)
-classification_rbf = SVC(kernel="rbf", )
+classification_rbf = SVC(kernel="rbf")
 classification_rbf.fit(x_train, y_train)
 predicts_rbf = classification_rbf.predict(x_test)
 
-#import statsmodels.formula.api as sm 
-#r_ols = sm.OLS(endog=y, exog=x)
-#r= r_ols.fit()
-#print(r)
 
 #CONFUSION MATRIX - RBF SVM
 
@@ -121,11 +138,5 @@ print("Confusion Matrix (RBF Kernel):\n"+str(cm_rbf))
 plot_confusion_matrix(cm_rbf, class_names)
 #print("Accuracy: "+str( (cm_rbf[0][0]+cm_rbf[1][1]) / (sum(cm_rbf[0])+sum(cm_rbf[1])) ) )
 print("Test Accuracy of SVM Algorithm (RBF): {:.2f}%".format(classification_rbf.score(x_test,y_test)*100))
-
-# R2 SCORE
-linear_r2 = r2_score(y_test, predicts_ln)
-rbf_r2 = r2_score(y_test, predicts_rbf)
-
-print("Linear SVM R2 degeri:"+str(linear_r2))
-print("Linear SVM R2 degeri:"+str(rbf_r2))
+print("TPR rate (Sensitivity-Recall) : "+ str(cm_rbf[1][1]) +" / "+str(cm_rbf[1][0]+cm_rbf[1][1])+" = "+str(cm_rbf[1][1]/(cm_rbf[1][0]+cm_rbf[1][1]) )  ) 
 
